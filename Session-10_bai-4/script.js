731f2593bac790fb504d0cd7bbaceb9c8fae4e79
nguy3ndat203 <tiendat12a1228@gmail.com>
2026-06-19 06:23:14 +0700

    Add files via upload

---
+++ Session-10_bai-4/script.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt('Nhập vào số thứ nhất: '))
+let secondNumber = Number(prompt('Nhập vào số thứ hai: '))
+
+console.log(`Tổng của ${firstNumber} và ${secondNumber} là: ${firstNumber + secondNumber}`)
