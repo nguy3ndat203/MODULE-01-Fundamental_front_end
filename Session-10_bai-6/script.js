b68859256c99be42cced659bd2ba764bca8f474b
nguy3ndat203 <tiendat12a1228@gmail.com>
2026-06-19 06:35:47 +0700

    Add files via upload

---
+++ Session-10_bai-6/script.js
@@ -0,0 +1,6 @@
+var mathPoints = 9.5
+var physicPoints = 8.0
+var chemistryPoints = 7.5
+
+var averagePoints = (mathPoints + physicPoints + chemistryPoints) / 3
+alert('Điểm trung bình: ' + averagePoints.toFixed(2))
